49f486ee9babc95a04d8d3033dcdbceb7981c6c0
fernando <fer_87_5@hotmail.com>
2021-03-11 12:38:52 -0300

    test: ensure DbAddAccount throws if encrypter throws

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,8 +8,8 @@ export class DbAddAccount implements AddAccount{
     this.encrypter = encrypter
   }
 
-  add (account: AddAccountModel): Promise<AccountModel>{
-    this.encrypter.encrypt(account.password)
+  async add (account: AddAccountModel): Promise<AccountModel>{
+    await this.encrypter.encrypt(account.password)
     return new Promise(resolve => resolve(null))
   }
 }
