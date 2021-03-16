22be3d20042d65a0ffaab9e3905fdac645f6ca07
fernando <fer_87_5@hotmail.com>
2021-03-16 18:31:12 -0300

    refactor: move data layer interfaces to folders

--- src/infra/criptograohy/bcrypt-adapter.ts
+++ src/infra/criptograohy/bcrypt-adapter.ts
@@ -1,12 +1,13 @@
-import { Encrypter } from '../../data/protocols/encrypter'
+import { Encrypter } from '../../data/protocols/criptography/encrypter'
 import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number
-  constructor(salt: number) {
+  constructor (salt: number) {
     this.salt = salt
   }
-  async encrypt(value: string): Promise<string> {
+
+  async encrypt (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
     return hash
   }
