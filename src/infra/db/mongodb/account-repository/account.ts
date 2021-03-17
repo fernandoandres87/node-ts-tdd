30616b8561c63a5a4c0cd07be4522a250339b12a
fernando <fer_87_5@hotmail.com>
2021-03-17 15:35:14 -0300

    feat: ensure AccountMongoRepository return an account on LoadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -9,4 +9,10 @@ export class AccountMongoRepository implements AddAccountRepository {
     const result = await accountCollection.insertOne(accountData)
     return MongoHelper.mapping(result.ops[0])
   }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.mapping(account)
+  }
 }
