7bac067d45352792ac33fd93970588367cf9c80e
fernando <fer_87_5@hotmail.com>
2021-03-18 16:24:02 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -2,13 +2,9 @@ import { badRequest, okRequest, serverError, unauthorized } from '../../helpers/
 import { Controller, HttpRequest, HttpResponse, Authentication, Validation } from './login-controller-protocols'
 
 export class LoginController implements Controller {
-  private readonly validation: Validation
-  private readonly authentication: Authentication
-
-  constructor (validation: Validation, authentication: Authentication) {
-    this.validation = validation
-    this.authentication = authentication
-  }
+  constructor (
+    private readonly validation: Validation,
+    private readonly authentication: Authentication) {}
 
   async handle (httpRequets: HttpRequest): Promise<HttpResponse> {
     try {
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -3,13 +3,9 @@ import { badRequest, serverError, okRequest } from '../../helpers/http/http-help
 import { Validation } from '../../protocols/validation'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-
-  constructor (addAccount: AddAccount, validation: Validation) {
-    this.addAccount = addAccount
-    this.validation = validation
-  }
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation) {}
 
   async handle (httpRequets: HttpRequest): Promise<HttpResponse> {
     try {
