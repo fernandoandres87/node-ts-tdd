ac7e74902a39a178ba40657af4d5fffbd6508796
fernando <fer_87_5@hotmail.com>
2021-03-19 11:40:16 -0300

    refactor: change route to email-validator-adapter

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,16 +3,15 @@ import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   test('Should return default content type as json', async () => {
-    app.get('/test_content_type', (req, res) =>{
+    app.get('/test_content_type', (req, res) => {
       res.send('')
     })
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
-  
   test('Should return xlm content type when forced', async () => {
-    app.get('/test_content_type_xml', (req, res) =>{
+    app.get('/test_content_type_xml', (req, res) => {
       res.type('xml')
       res.send('')
     })
