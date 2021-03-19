7efd66ae7d91d032c055e2bacab0a6ea068f2f96
fernando <fer_87_5@hotmail.com>
2021-03-19 11:07:13 -0300

    refactor: rename files and move into folder

--- src/main/adapters/validators/email-validator-adapter.spec.ts
+++ src/main/adapters/validators/email-validator-adapter.spec.ts
@@ -2,7 +2,7 @@ import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
-  isEmail(): boolean {
+  isEmail (): boolean {
     return true
   }
 }))
