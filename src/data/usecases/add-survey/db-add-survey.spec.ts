4c97bdc3ccdbe49f3e18144f6291f6313900679a
fernando <fer_87_5@hotmail.com>
2021-03-25 16:26:36 -0300

    feat: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,26 @@
+import { DbAddSurvey } from './db-add-survey'
+import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }]
+})
+
+describe('DbAddSurvey UseCase', () => {
+  test('Should call AddSurveyRepository with correct values', async () => {
+    class AddSurveyRespositoryStub implements AddSurveyRepository {
+      async add (add: AddSurveyModel): Promise<void> {
+        return new Promise(resolve => resolve())
+      }
+    }
+    const addSurveyRepositoryStub = new AddSurveyRespositoryStub()
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
+    const sut = new DbAddSurvey(addSurveyRepositoryStub)
+    const surveyData = makeFakeSurveyData()
+    await sut.add(surveyData)
+    expect(addSpy).toHaveBeenCalledWith(surveyData)
+  })
+})
---
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -0,0 +1,10 @@
+import { AddSurveyRepository } from '../../protocols/db/survey/add-survey-repository'
+import { AddSurvey, AddSurveyModel } from './db-add-survey-protocols'
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (private readonly addSurveyRespository: AddSurveyRepository) {}
+
+  async add (data: AddSurveyModel): Promise<void> {
+    await this.addSurveyRespository.add(data)
+  }
+}
