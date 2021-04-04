dbe2b7bb79417679db79a11d47a8856a79a7650c
fernando <fer_87_5@hotmail.com>
2021-04-04 14:46:29 -0300

    feat: ensure load surveys routes returns 403 if no access token is provider

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
+import { LoadSurveyRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
 import { AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
 import { AddSurveyModel } from '../../../../domain/useCases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveyRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys: SurveyModel[] = await surveyCollection.find().toArray()
+    return surveys
   }
 }
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 import { makeAddSurveyController } from '../factories/controller/survey/add-survey/add-survey-controller-factory'
+import { makeLoadSurveysController } from '../factories/controller/survey/load-surveys/load-surveys-controller-factory'
 import { makeAuthMiddleware } from '../factories/middleware/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
+
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
