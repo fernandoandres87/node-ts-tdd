6873f9a413fda81f1df7a2f2a776e110c06b3bdb
fernando <fer_87_5@hotmail.com>
2021-04-04 21:24:30 -0300

    fix: make controllers return the error message instead of the error name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 4000,
   jwtSecret: process.env.JWT_SECRET || 'usdjfJFJAOSPOs92182934)"$/#"/%ASDJJ'
 }
