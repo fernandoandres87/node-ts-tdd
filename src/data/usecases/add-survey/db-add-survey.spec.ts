3a1728bd7f8d8978f8a3728eed89d816612572f0
Fernando Lopez <fernandol@fktech.net>
2021-05-25 21:27:20 -0300

    test: ensure DbSaveSurveyResult throws if SaveSurveyResutlRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -50,7 +50,7 @@ describe('DbAddSurvey UseCase', () => {
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
   })
-  test('Should call AddSurveyRepository with correct values', async () => {
+  test('Should throw if AddSurveyRepository throws', async () => {
     const { sut, addSurveyRepositoryStub } = makeSut()
     jest.spyOn(addSurveyRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.add(makeFakeSurveyData())
